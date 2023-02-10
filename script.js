09e58f9fa6f41052563c29e16cdf9de558682e58
shivam <shivampandey870@gmail.com>
2023-02-10 03:59:16 -0800

    new update

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
-function initialize(){
-    var html=document.getElementById("html");
-    var css=document.getElementById("css");
-    var js=document.getElementById("js");
-    var code=document.getElementById("code").contentWindow.document;
-    document.body.onkeyup=function(){
-        code.open();
-        code.writeln(
-         html.value +
+"use strict";
 
-        "<style>"+ css.value+"</style>"  +
+(function() {
+  var html = document.getElementById("html");
+  var css = document.getElementById("css");
+  var js = document.getElementById("js");
+  var code = document.getElementById("code").contentWindow.document;
 
-         "<script>"+js.value+"</script>"
-
-        );
-        code.close();
-    }
-}
-initialize();
+  document.body.addEventListener("keyup", function() {
+    code.open();
+    code.write(
+      html.value +
+        "<style>" +
+        css.value +
+        "</style>" +
+        "<script>" +
+        js.value +
+        "</script>"
+    );
+    code.close();
+  });
+})();
